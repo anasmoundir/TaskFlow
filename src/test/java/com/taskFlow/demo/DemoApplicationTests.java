c5862bd94b05fde7d32de36132e543ceef64d616
anasmoundir <anasmoundir1996@gmail.com>
2023-12-25 08:51:54 +0100

    FirstPush

---
+++ src/test/java/com/taskFlow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskFlow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
